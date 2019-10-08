4124d30db8699d1639f364481e850f52450e575e
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-09 00:03:25 +0200

    print test null char

--- res/include/ud_array.h
+++ res/include/ud_array.h
@@ -17,14 +17,14 @@
 # define ud_arr_free(w)         ud_arr_free_r(w, 0)
 # define ud_arr_rfree(w)         ud_arr_free_r(w, -1)
 
-// # define ud_arr_set(type, ...) ({ ud_arr *new_arr; type in_val[UD_ARGS_LEN(type, __VA_ARGS__)] = {__VA_ARGS__}; size_t len = sizeof(in_val) / sizeof(type); new_arr = ud_arr_init(sizeof(type), len); type *val = (type*)new_arr->val; for (ud_ut_count i = 0; i < len; ++i) val[i] = in_val[i]; new_arr; })
+// # define ud_arr_set(type, ...) ({ ud_arr *new_arr; type in_val[] = {__VA_ARGS__}; size_t len = sizeof(in_val) / sizeof(type); new_arr = ud_arr_init(sizeof(type), len); type *val = (type*)new_arr->val; for (ud_ut_count i = 0; i < len; ++i) val[i] = in_val[i]; new_arr; })
 
 
 # define ud_arr_set(type, ...) \
     ({ \
         ud_arr *new_arr; \
         type in_val[] = {__VA_ARGS__}; \
-        printf("add set = %p\n", in_val[0]); \
+        printf("add set = %s\n", #type); \
         size_t len = sizeof(in_val) / sizeof(type); \
         new_arr = ud_arr_init(sizeof(type), len); \
         type *val = (type*)new_arr->val; \
