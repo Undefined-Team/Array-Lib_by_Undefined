a5d34f24c7cd1fb28114cc0b30c8c8ec81f9b902
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-08 18:49:20 +0200

    print test null char

--- res/src/print.c
+++ res/src/print.c
@@ -88,13 +88,13 @@ char            **ud_arr_print_get_arr(ud_arr *arr, char *format)
     UD_UT_PROT_MALLOC(total_arr = ud_ut_malloc(sizeof(char) * (total_size + 1)));
     printf("total size = %zd && save_len = %zd\n", total_size, save_len);
     total_arr[total_size] = '\0';
+    count = save_len;
+    (void)ud_arr_print_get_total_arr(arr, total_arr, format, format_len, 0, &count);
     if (save_len == 0)
     {
         printf("%s", total_arr);
         return NULL;
     }
-    count = save_len;
-    (void)ud_arr_print_get_total_arr(arr, total_arr, format, format_len, 0, &count);
     // real_arr[0] = &total_arr[index_cut[0]];
     for (ud_ut_count i = 0; i < save_len; ++i)
         real_arr[i] = &total_arr[index_cut[i]];
